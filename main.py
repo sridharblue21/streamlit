3f0754c0e4ded9df6f95a58d3f92ea8608395229
vrlnarayana <vrl.narayana@gmail.com>
2021-01-08 22:44:27 +0530

    Changes 4jan. Added sub page - popular rating based recommendation

--- main.py
+++ main.py
@@ -1,46 +1,44 @@
 import streamlit as st
-import stapp #import sub-module stapp
+import stapp # import sub-module stapp
 from load_css import local_css
 import login
 import authenticate
 import popular_reco
 import menu
-import numpy as np
-local_css("style.css") #include style.css
+
+local_css("style.css") # include style.css
 
 def print_hi(name):
-    #st.header('Welcome to Group 4-Capstone Project')
     welcome_head=f"<div>Hi <span class='highlight blue'>{name}</span>, Welcome!</div>"
     st.markdown(welcome_head,unsafe_allow_html=True)
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
+def main():
     yourname,yourpass=login.login() #get login field values
     st.header('Top Songs Recommender System ')
-    message=authenticate.authenticate(yourname,yourpass) # authenticate with name,passcode not empty and passcode matching
-    if message == 'authenticated': #display blocks below if authenticated
+    message=authenticate.authenticate(yourname,yourpass)
+    # authenticate with name,passcode not empty and passcode matching
+    if message == 'authenticated': # display blocks below if authenticated
         print_hi(yourname)
         st.write ('\n')
         menu_out=menu.menu()
 
         if menu_out=='Artist Choice':
-            #menu_mesg=stapp.func_welcome(menu_out,1)# call welcome function from the sub-module
-            #st.markdown(menu_mesg, unsafe_allow_html=True)
+            # menu_mesg=stapp.func_welcome(menu_out,1)# call welcome function from the sub-module
+            # st.markdown(menu_mesg, unsafe_allow_html=True)
             pop=popular_reco.artist_choice()
             if pop:
                 pop_msg=f"<div>Your favourite(s), <span class='highlight blue'>{pop}</span></div>"
                 st.markdown(pop_msg,unsafe_allow_html=True)
             st.write('\n')
         elif menu_out=='Popular Titles':
-            #menu_mesg=stapp.func_welcome(menu_out,1)# call welcome function from the sub-module
-            #st.markdown(menu_mesg, unsafe_allow_html=True)
+            # menu_mesg=stapp.func_welcome(menu_out,1)# call welcome function from the sub-module
+            # st.markdown(menu_mesg, unsafe_allow_html=True)
 
             #sub_menu popularity menus
             sub_menu_out=menu.submenu_1()
             if sub_menu_out=='Most Played':
-                #sub_menu_mesg = stapp.func_welcome(sub_menu_out,2)  # call welcome function from the sub-module
-                #st.markdown(sub_menu_mesg, unsafe_allow_html=True)
+                # sub_menu_mesg = stapp.func_welcome(sub_menu_out,2)  # call welcome function from the sub-module
+                # st.markdown(sub_menu_mesg, unsafe_allow_html=True)
                 st.write('\n')
                 n=st.slider('Select how many song recommendations you would need?',min_value=2, max_value=10)
                 top_pop_songs_title = stapp.top_pop_songs(n)# call popular songs function top-5
@@ -50,16 +48,25 @@ if __name__ == '__main__':
                     st.table(top_pop_songs_title)
                     st.success('Done')
             elif sub_menu_out == 'Most Rated':
-                #sub_menu_mesg = stapp.func_welcome(sub_menu_out,2)  # call welcome function from the sub-module
+                #sub_menu_mesg = stapp.func_welcome(sub_menu_out,2)
                 #st.markdown(sub_menu_mesg, unsafe_allow_html=True)
                 st.write('\n')
-                n=st.slider('Select how many song recommendations you would need?',min_value=2, max_value=10)
-                top_rated_songs_title = stapp.top_rated_songs(n)# call popular songs function top-5
-                st.subheader(f"Most rated titles, {n} recommendations for you")
-                st.spinner()
-                with st.spinner(text='In progress'):
-                    st.table(top_rated_songs_title)
-                    st.success('Done')
+                region=st.selectbox('Choose your region',options=['IN','US','/N'])
+                if region:
+                    n=st.slider('Select how many song recommendations you would need?',min_value=2, max_value=10)
+                    top_rated_songs_title = stapp.top_rated_songs(n, region)  # call popular songs function top-5
+                    st.subheader(f"Most rated titles, {n} recommendations for you")
+                    st.write(id)
+                    st.spinner()
+                    with st.spinner(text='In progress'):
+                        st.table(top_rated_songs_title)
+                        st.success('Done')
+                else:
+                    st.write('Choose your region')
             st.write('\n')
     else:
         st.sidebar.markdown(message,unsafe_allow_html=True)
+
+# call main function here
+if __name__ == '__main__':
+    main()
--- stapp.py
+++ stapp.py
@@ -9,16 +9,20 @@ import readdata
 local_css('style.css')
 
 
-def func_welcome(menu_in,level=1): # menu_in is menu name, level is signify if its main menu or submenu (only two level possible now)
-    if level==1:
+def func_welcome(menu_in, level=1):
+    # menu_in is menu name, level is signify if its main menu or submenu (only two level possible now)
+    if level == 1:
         menu_out_msg = f"<div><span class='highlight red'>{menu_in}</span>>>>>></div>"
-    elif level==2:
+    elif level == 2:
         menu_out_msg = f"<div> >>>>>>>>> <span class='highlight lblue'>{menu_in}</span></div>"
     return menu_out_msg
 
-def fn_markdown(choice1): #global markdown function, can be called from anywhere
-    choice_out=st.markdown(f"<div><span class='highlight blue'>{choice1}</span></div>",unsafe_allow_html=True)
+
+def fn_markdown(choice1):
+    #global markdown function, can be called from anywhere
+    choice_out = st.markdown(f"<div><span class='highlight blue'>{choice1}</span></div>", unsafe_allow_html=True)
     return choice_out
+
 
 def fn_songs_play_count():
     song_data = readdata.read_song()
@@ -28,13 +32,15 @@ def fn_songs_play_count():
                                                                                             ascending=False)
     sorted_play_counts_df.reset_index(inplace=True)
     sorted_play_counts_df.set_index('song_id',
-                                    inplace=True)  # making song_id index of the df, this is required to merge with song_data df
+                                    inplace=True)
+    # making song_id index of the df, this is required to merge with song_data df
 
     # Sorted in descending order of play_count to see the top play counts first
     songs_play_count_df = pd.merge(sorted_play_counts_df, song_data, on='song_id')
-    return songs_play_count_df,song_data,count_data
+    return songs_play_count_df, song_data, count_data
 
-def top_pop_songs(n): #top popular songs recommendation by playcount
+
+def top_pop_songs(n): #top popular songs recommendation by play count
     songs_play_count_df, song_data, count_data = fn_songs_play_count()
     top_count_data = count_data[count_data.play_count > 20]
 
@@ -42,25 +48,37 @@ def top_pop_songs(n): #top popular songs recommendation by playcount
     count_play_count = songs_play_count_df.groupby('song_id').count()['play_count']
     final_play_count = pd.DataFrame({'sum_play_count': sum_play_count, 'count_play_count': count_play_count})
     #Top n popular songs from song data for new users
-    top_pop_songs_id=list(popular_reco.top_played_n_songs(final_play_count, n, min_interaction=50))
+    top_pop_songs_id = list(popular_reco.top_played_n_songs(final_play_count, n, min_interaction=50))
     choices=np.unique(song_data[song_data.song_id.isin(top_pop_songs_id)].title.values)+' by '+np.unique(song_data[song_data.song_id.isin(top_pop_songs_id)].artist_name.values)
-    top_pop_songs_title=pd.DataFrame(choices,[x for x in range(1,n+1)]).reset_index()
-    top_pop_songs_title.columns=['S.No','Titles']
-    top_pop_songs_title.set_index('S.No',inplace=True)
+    top_pop_songs_title = pd.DataFrame(choices, [x for x in range(1, n+1)]).reset_index()
+    top_pop_songs_title.columns = ['S.No', 'Titles']
+    top_pop_songs_title.set_index('S.No', inplace=True)
     return top_pop_songs_title
 
-def top_rated_songs(n): #top rated songs recommendation (combining IMDb dataset)
+def top_rated_songs(n,region): #top rated songs recommendation (combining IMDb dataset)
     df_akas_data = readdata.read_akas()
     df_rating_data = readdata.read_rating()
     songs_play_count_df, song_data, count_data = fn_songs_play_count()
     imdb_merge = pd.merge(df_akas_data, df_rating_data, left_on='titleId', right_on='tconst')
-    imdb_merge = imdb_merge[imdb_merge.ordering == 1]  # filtering further to remove duplicate titles
+    imdb_merge = imdb_merge[(imdb_merge.ordering == 1)&(imdb_merge.region==region)]  # filtering further to remove duplicate titles
     song_imdb_merge = pd.merge(songs_play_count_df, imdb_merge, left_on='title',
                                right_on='title')  # Song data and IMDb rating data merged
     song_imdb_merge.set_index('song_id', inplace=True)
-    top_rated_songs_id=list(popular_reco.top_rated_n_songs(song_imdb_merge, n, min_interaction=20))  #min_interaction=20 is input for numVotes
-    choices=np.unique(song_data[song_data.song_id.isin(top_rated_songs_id)].title.values)+' by '+np.unique(song_data[song_data.song_id.isin(top_rated_songs_id)].artist_name.values)
-    top_rated_songs_title=pd.DataFrame(choices,[x for x in range(1,n+1)]).reset_index()
-    top_rated_songs_title.columns=['S.No','Titles']
-    top_rated_songs_title.set_index('S.No',inplace=True)
-    return top_rated_songs_title
+    # min_interaction=20 is input for numVotes
+    numVotes = song_imdb_merge.groupby(['song_id', 'region'])['numVotes'].agg(['sum']).sort_values(by=['sum'],
+                                                                                                   ascending=False)
+    avgRating = song_imdb_merge.groupby(['song_id', 'region'])['averageRating'].agg(['mean']).sort_values(by=['mean'],
+                                                                                                          ascending=False)
+    final_rating_Votes = pd.merge(numVotes.reset_index(), avgRating.reset_index(),
+                                  on=['song_id', 'region']).sort_values(by='sum', ascending=False)
+    final_rating_Votes.columns = ['song_id', 'region', 'numVotes', 'averageRating']
+    final_rating_Votes = final_rating_Votes.set_index('song_id')
+
+    top_rated_songs_id = list(popular_reco.top_rated_n_songs(final_rating_Votes, n, min_interaction=20))
+    #choices = np.unique(song_data[song_data.song_id.isin(top_rated_songs_id)].title.values)+' by '+np.unique(song_data[song_data.song_id.isin(top_rated_songs_id)].artist_name.values)
+    choices = song_data[song_data.song_id.isin(top_rated_songs_id)][['title','release','artist_name']]
+    #top_rated_songs_title = pd.DataFrame(choices, [x for x in range(1, n+1)]).reset_index()
+    choices['S.No']=[x for x in range(1, len(choices)+1)]
+    #top_rated_songs_title.columns = ['S.No', 'Titles','Release','Artist Name']
+    choices.set_index('S.No', inplace=True)
+    return choices
